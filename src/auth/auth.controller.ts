5ac38ae531c967a1c7e8c912fa607420f2bd3a4e
“KazukiTakeuchi” <takeuchi1227813813@gmail.com>
2023-05-25 08:02:34 +0900

    feat: login, logoutが可能になった

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -21,4 +21,36 @@ export class AuthController {
   signUp(@Body() dto: AuthDto): Promise<Msg> {
     return this.authService.signUp(dto);
   }
+
+  @HttpCode(HttpStatus.OK)
+  @Post('login')
+  async login(
+    @Body() dto: AuthDto,
+    @Res({ passthrough: true }) res: Response,
+  ): Promise<Msg> {
+    const jwt = await this.authService.login(dto);
+    res.cookie('access_token', jwt.accessToken, {
+      httpOnly: true,
+      secure: false, // ローカルで動作確認にするために一時的にfalseにしています
+      sameSite: 'none',
+      path: '/',
+    });
+    return {
+      message: 'ok',
+    };
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Post('logout')
+  logout(@Req() req: Request, @Res({ passthrough: true }) res: Response): Msg {
+    res.cookie('access_token', '', {
+      httpOnly: true,
+      secure: false, // ローカルで動作確認にするために一時的にfalseにしています
+      sameSite: 'none',
+      path: '/',
+    });
+    return {
+      message: 'ok',
+    }
+  }
 }
