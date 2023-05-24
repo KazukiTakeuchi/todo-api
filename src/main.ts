40995da96833138770bdfe98239234ceda8ca652
“KazukiTakeuchi” <takeuchi1227813813@gmail.com>
2023-05-24 16:55:25 +0900

    chore: アプリの骨組みを作成

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Request } from 'express';
+import { ValidationPipe } from '@nestjs/common';
+import cookieParser from 'cookie-parser';
+import csurf from 'csurf';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.enableCors({
+    credentials: true,
+    origin: ['http://localhost:3000'],
+  });
+  app.use(cookieParser());
   await app.listen(3005);
 }
 bootstrap();
+  
