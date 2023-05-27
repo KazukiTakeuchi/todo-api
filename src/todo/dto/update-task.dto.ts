9ae7559647b54a8d955a5707c32a6565f44995c4
“KazukiTakeuchi” <takeuchi1227813813@gmail.com>
2023-05-27 13:21:18 +0900

    fix: スペルミスがあったので修正

--- src/todo/dto/update-task.dto.ts
+++ src/todo/dto/update-task.dto.ts
@@ -3,7 +3,7 @@ import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
 export class UpdateTaskDto {
   @IsString()
   @IsNotEmpty()
-  titiel: string;
+  title: string;
 
   @IsString()
   @IsOptional()
